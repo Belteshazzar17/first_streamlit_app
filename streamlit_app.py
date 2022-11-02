6deba7955d84ebf2bad7c04cdb4562176083be3c
Belteshazzar Luwalekeah <daniel.cooke900@gmail.com>
2022-11-02 11:16:09 -0600

    Update streamlit_app.py
    trying the column element of the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ streamlit.header("View Our Fruit List - Add your Favorites!")
 # Snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("SELECT FRUIT_NAME FROM FRUIT_LOAD_LIST")
+    my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
     return my_cur.fetchall()
 
 # Add a button to load the fruit
@@ -58,7 +58,7 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
-  streamlit.dataframe(my_data_rows)
+  streamlit.dataframe(my_data_rows, columns=["Fruit Names"])
 
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
