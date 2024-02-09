a0cea4bc47764811814918542e7b88d817fdf541
Belteshazzar Luwalekeah <daniel.cooke900@gmail.com>
2024-02-08 23:23:54 -0700

    actually commented out the lines that adds an entry to a snowflake db since its not setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,19 +60,17 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
-"""
+
 # I am commenting this code out because i dont ahve access to a snowflake db, maybe i can use Azure?
 
 # Allow the end user to add a fruit to the list
-def insert_row_snowflake(new_fruit):
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('" + new_fruit +"')")
-    return "Thanks for adding " + new_fruit
+# def insert_row_snowflake(new_fruit):
+#   with my_cnx.cursor() as my_cur:
+#     my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('" + new_fruit +"')")
+#     return "Thanks for adding " + new_fruit
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
-if streamlit.button('Add a Fruit to the list'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  back_from_function = insert_row_snowflake(add_my_fruit)
-  streamlit.text(back_from_function)
-
-"""
+# add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
+# if streamlit.button('Add a Fruit to the list'):
+#   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#   back_from_function = insert_row_snowflake(add_my_fruit)
+#   streamlit.text(back_from_function)
