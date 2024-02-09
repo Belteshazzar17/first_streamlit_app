980f445a58d055fcd2811c15a9fbefc9855018b0
Belteshazzar Luwalekeah <daniel.cooke900@gmail.com>
2024-02-08 22:51:28 -0700

    commented out the code to add fruits to the database bc my snowflake trial ended

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,9 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
+"""
+# I am commenting this code out because i dont ahve access to a snowflake db, maybe i can use Azure?
+
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
@@ -71,3 +74,5 @@ if streamlit.button('Add a Fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
+
+"""
