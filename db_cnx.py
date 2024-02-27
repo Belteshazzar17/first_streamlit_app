6ff61ad05a9f4a745866e0a12612c770f34892fe
Daniel L. Cooke <daniel.cooke900@gmail.com>
2024-02-26 21:46:36 -0700

    Update db_cnx.py
    connecting a simpler way to ensure i can connect from the streamlit docs

--- db_cnx.py
+++ db_cnx.py
@@ -4,9 +4,19 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+
+# Initialize connection.
+conn = streamlit.connection("snowflake")
+
+# Perform query.
+df = conn.query("SELECT * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST;", ttl=600)
+
+# Print results.
+st.write(df)
+
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
